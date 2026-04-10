71b1a4dd2ec7376739e8896f1b2e1d7c66ba52e3
tknkaa <tknkaa@users.noreply.github.com>
2026-04-10 15:57:34 +0800

    set up biome and format

--- app/lib/db/schema.ts
+++ app/lib/db/schema.ts
@@ -1,24 +1,24 @@
-import { sql } from 'drizzle-orm';
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable('users', {
-  id: integer('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').unique().notNull(),
+export const users = sqliteTable("users", {
+	id: integer("id").primaryKey(),
+	name: text("name").notNull(),
+	age: integer("age").notNull(),
+	email: text("email").unique().notNull(),
 });
 
-export const posts = sqliteTable('posts', {
-  id: integer('id').primaryKey(),
-  title: text('title').notNull(),
-  content: text('content').notNull(),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => users.id, { onDelete: 'cascade' }),
-  createdAt: text('created_at')
-    .default(sql`(CURRENT_TIMESTAMP)`)
-    .notNull(),
-  updatedAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(() => new Date()),
+export const posts = sqliteTable("posts", {
+	id: integer("id").primaryKey(),
+	title: text("title").notNull(),
+	content: text("content").notNull(),
+	userId: integer("user_id")
+		.notNull()
+		.references(() => users.id, { onDelete: "cascade" }),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	updatedAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+		() => new Date(),
+	),
 });
 
 export type InsertUser = typeof users.$inferInsert;
--- drizzle.config.remote.ts
+++ drizzle.config.remote.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'drizzle-kit';
-import { config } from "dotenv"
+import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
 
-config({ path: ".env.production" })
+config({ path: ".env.production" });
 
 export default defineConfig({
-  schema: './app/lib/db/schema.ts',
-  out: './migrations',
-  dialect: 'turso',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-    authToken: process.env.DATABASE_AUTH_TOKEN!,
-  },
+	schema: "./app/lib/db/schema.ts",
+	out: "./migrations",
+	dialect: "turso",
+	dbCredentials: {
+		url: process.env.DATABASE_URL!,
+		authToken: process.env.DATABASE_AUTH_TOKEN!,
+	},
 });
