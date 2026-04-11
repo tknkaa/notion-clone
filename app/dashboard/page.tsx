2d38cade62448d9dff2c71a1176d5c4d16c6e43b
tknkaa <tknkaa@users.noreply.github.com>
2026-04-11 10:11:50 +0800

    check user when accessing dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,11 @@
+import { headers } from "next/headers";
+import { auth } from "../lib/auth";
+
 //TODO: check user
-export default function Page() {
+export default async function Page() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+	console.log(session);
 	return <h1>Dashboard</h1>;
 }
