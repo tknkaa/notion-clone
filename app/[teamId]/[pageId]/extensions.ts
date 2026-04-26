8c69bead70bbd4a56edc4b7a14576a014b477bef
tknkaa <tknkaa@users.noreply.github.com>
2026-04-26 19:02:13 +0800

    start math mode with $

--- app/[teamId]/[pageId]/extensions.ts
+++ app/[teamId]/[pageId]/extensions.ts
@@ -1,4 +1,4 @@
-import { Node, mergeAttributes } from "@tiptap/core";
+import { Node, mergeAttributes, InputRule } from "@tiptap/core";
 import { ReactNodeViewRenderer } from "@tiptap/react";
 import MathView from "./MathView";
 
@@ -20,6 +20,23 @@ export const Math = Node.create({
 		return {
 			latex: { default: "" },
 		};
+	},
+
+	addInputRules() {
+		return [
+			new InputRule({
+				find: /\$([^$]+)\$$/,
+				handler: ({ state, range, match }) => {
+					const { tr } = state;
+					const latex = match[1];
+					tr.replaceWith(
+						range.from,
+						range.to,
+						state.schema.nodes.math.create({ latex }),
+					);
+				},
+			}),
+		];
 	},
 
 	parseHTML() {
